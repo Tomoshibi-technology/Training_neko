1662fe6a859cbb29ac8ef595f457f9cae9050830
Neko-neko-Ne <111097589+Neko-neko-Ne@users.noreply.github.com>
2024-06-08 16:37:24 +0900

    move関数

--- ftomo-robot-test2/src/main.cpp
+++ ftomo-robot-test2/src/main.cpp
@@ -13,25 +13,33 @@ int strtSW_pin = 14;
 int strtSW = 0;
 int go_dig = 0;
 int go_sp = 0;
+int n = 0;
 
-int drvDigSp(int mtrNum, int dig, int sp) {
-  int drv = sin((dig + (135.0 - mtrNum * 90.0)) / 180.0 * 3.14) * sp;
+int drvDigSp(int mtrNum, int moveDig, int moveSp) {
+  moveDig = (moveDig + 180) % 360 - 180;
+  int drv = sin((moveDig + (135.0 - mtrNum * 90.0)) / 180.0 * 3.14) * moveSp;
   return drv;
 }
 
-void drvMotor(int pinA, int pinB, int sp) {
-  if(sp > 0) {
-    analogWrite(pinA, sp);
+void drvMotor(int pinA, int pinB, int mtrSp) {
+  if(mtrSp > 0) {
+    analogWrite(pinA, mtrSp);
     analogWrite(pinB, 0);
-  } else if (sp < 0) {
+  }else if(mtrSp < 0) {
     analogWrite(pinA, 0);
-    analogWrite(pinB, abs(sp));
-  } else {
+    analogWrite(pinB, abs(mtrSp));
+  }else{
     analogWrite(pinA, 0);
     analogWrite(pinB, 0);
   }
 }
 
+void move(int mtrNum, int pinA, int pinB, int moveDig, int moveSp){
+  int mtrSp = drvDigSp(mtrNum, moveDig, moveSp);
+  drvMotor(pinA, pinB, mtrSp);
+}
+
+//--------------------------------------------------------------------------------------
 void setup() {
   pinMode(21, OUTPUT);
   pinMode(22, OUTPUT);
@@ -41,28 +49,19 @@ void setup() {
 }
 
 void loop() {
+  n++;
+  go_dig = n / 100;
+  go_sp = 50;
+
   strtSW = digitalRead(strtSW_pin);
-  
-  go_dig = 0;
-  go_sp = 100;
-
-  int sp0 = drvDigSp(0, go_dig, go_sp);
-  int sp1 = drvDigSp(1, go_dig, go_sp);
-  int sp2 = drvDigSp(2, go_dig, go_sp);
-  int sp3 = drvDigSp(3, go_dig, go_sp);
-
-  if(strtSW == 1){
-    sp0 = sp0;
-    sp1 = sp1;
-    sp2 = sp2;
-    sp3 = sp3;
-  } else {
-    sp0 = sp1 = sp2 = sp3 = 0;
+  if(strtSW == 0){
+    go_sp = go_sp;
+  }else{
+    go_sp = 0;
   }
 
-  drvMotor(M0A, M0B, sp0);
-  drvMotor(M1A, M1B, sp1);
-  drvMotor(M2A, M2B, sp2);
-  drvMotor(M3A, M3B, sp3);
-
+  move(0, M0A, M0B, go_dig, go_sp);
+  move(1, M1A, M1B, go_dig, go_sp);
+  move(2, M2A, M2B, go_dig, go_sp);
+  move(3, M3A, M3B, go_dig, go_sp);
 }
